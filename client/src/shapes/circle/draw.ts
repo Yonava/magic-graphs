4c06937fd3637f10c8e625d4dd34772f89260adc
teykamp <thomas@eykamp.com>
2025-05-28 22:05:58 -0400

    return circle files

---
+++ client/src/shapes/circle/draw.ts
@@ -0,0 +1,26 @@
+import { CIRCLE_SCHEMA_DEFAULTS } from '@shape/circle';
+import type { CircleSchema } from '@shape/circle';
+
+export const drawCircleWithCtx =
+  (options: CircleSchema) => (ctx: CanvasRenderingContext2D) => {
+    const { at, radius, color, stroke } = {
+      ...CIRCLE_SCHEMA_DEFAULTS,
+      ...options,
+    };
+
+    ctx.beginPath();
+    ctx.arc(at.x, at.y, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+
+    if (stroke) {
+      const { color, width, dash } = stroke;
+      ctx.strokeStyle = color;
+      ctx.lineWidth = width;
+      ctx.setLineDash(dash || []);
+      ctx.stroke();
+      ctx.setLineDash([]);
+    }
+
+    ctx.closePath();
+  };
---
+++ client/src/shapes/circle/hitbox.ts
@@ -0,0 +1,45 @@
+import type { Coordinate, BoundingBox } from '@shape/types';
+import type { CircleSchema } from '@shape/circle';
+import { STROKE_DEFAULTS } from '@shape/types';
+import { rectEfficientHitbox } from '@shape/rect/hitbox';
+import { normalizeBoundingBox } from '@shape/helpers';
+
+export const circleHitbox = (circle: CircleSchema) => (point: Coordinate) => {
+  const dx = point.x - circle.at.x;
+  const dy = point.y - circle.at.y;
+
+  const stroke = {
+    ...STROKE_DEFAULTS,
+    ...circle.stroke,
+  };
+
+  const radiusWithStroke = circle.radius + stroke.width / 2;
+
+  return dx ** 2 + dy ** 2 <= radiusWithStroke ** 2;
+};
+
+export const getCircleBoundingBox = (circle: CircleSchema) => () => {
+  const { at, radius } = circle;
+
+  const { width: borderWidth } = {
+    ...STROKE_DEFAULTS,
+    ...circle.stroke,
+  };
+
+  return normalizeBoundingBox({
+    at: {
+      x: at.x - (radius + borderWidth / 2),
+      y: at.y - (radius + borderWidth / 2),
+    },
+    width: 2 * (radius + borderWidth / 2),
+    height: 2 * (radius + borderWidth / 2),
+  });
+};
+
+export const circleEfficientHitbox = (circle: CircleSchema) => {
+  const circleBoundingBox = getCircleBoundingBox(circle)();
+
+  const isInRectEfficientHitbox = rectEfficientHitbox(circleBoundingBox);
+
+  return (boxToCheck: BoundingBox) => isInRectEfficientHitbox(boxToCheck);
+};
