96513b9edfc35f3d9ba9f984ac2a65d7cade77bd
yonava <yonavossandreae@gmail.com>
2025-06-10 01:38:07 -0400

    track canvas box siz

--- client/src/canvas/index.ts
+++ client/src/canvas/index.ts
@@ -1,27 +1,34 @@
-import { onMounted, ref } from "vue"
+import { onMounted, ref, watch } from "vue"
 import { useCamera } from "./camera"
 import { useMagicCoordinates } from "./coordinates"
 import { getCtx } from "@utils/ctx"
 import { getDevicePixelRatio } from "./camera/utils"
 import type { MagicCanvasConfig, UseMagicCanvas } from "./types"
+import { useElementSize } from "@vueuse/core"
 
 const REPAINT_FPS = 60;
 
+const initCanvasWidthHeight = (canvas: HTMLCanvasElement | undefined) => {
+  if (!canvas) throw new Error('Canvas not found in DOM. Check ref link.');
+
+  const dpr = getDevicePixelRatio()
+  const rect = canvas.getBoundingClientRect();
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+}
+
 export const useMagicCanvas: UseMagicCanvas = (config: MagicCanvasConfig) => {
   const canvas = ref<HTMLCanvasElement>()
+  const canvasBoxSize = useElementSize(canvas)
 
   let repaintInterval: NodeJS.Timeout;
 
   onMounted(() => {
-    if (!canvas.value) throw new Error('Canvas not found in DOM. Check ref link.');
-
-    const dpr = getDevicePixelRatio()
-    const rect = canvas.value.getBoundingClientRect();
-    canvas.value.width = rect.width * dpr;
-    canvas.value.height = rect.height * dpr;
-
+    initCanvasWidthHeight(canvas.value)
     repaintInterval = setInterval(repaintCanvas, 1000 / REPAINT_FPS);
   })
+
+  watch([canvasBoxSize.width, canvasBoxSize.height], () => initCanvasWidthHeight(canvas.value))
 
   const { cleanup: cleanupCamera, ...camera } = useCamera(canvas);
   const { coordinates: cursorCoordinates, cleanup: cleanupCoords } = useMagicCoordinates(canvas);
