89de888313b2c1f9a63bc493b5b514ef63022915
yonava <yonavossandreae@gmail.com>
2025-06-20 12:48:49 +0200

    removed unused playground code

--- client/src/graphs/themes/usePreferredTheme.ts
+++ client/src/graphs/themes/usePreferredTheme.ts
@@ -3,7 +3,10 @@ import { useDark } from '@vueuse/core';
 import type { BaseGraph } from '@graph/base';
 import { useLocalStorage } from '@vueuse/core';
 import { THEME_NAMES, type GraphThemeName } from '.';
+// @typescript-eslint/no-unused-vars reports unused even if referenced in jsdoc
+// eslint-disable-next-line
 import type { Graph } from '@graph/types';
+import { localKeys } from '@utils/localStorage';
 
 export type PreferredGraphTheme = GraphThemeName | 'auto';
 
@@ -19,7 +22,7 @@ const DEFAULT_THEME: PreferredGraphTheme = 'auto';
 export const usePreferredTheme = (graph: BaseGraph) => {
   const isDark = useDark();
   const preferredTheme = useLocalStorage<PreferredGraphTheme>(
-    'preferred-theme',
+    localKeys.preferredTheme,
     DEFAULT_THEME,
   );
 
