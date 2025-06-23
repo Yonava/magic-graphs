010bc200aae7b06f9f7c45bea70b03d4e83fc274
yonava <yonavossandreae@gmail.com>
2025-06-23 23:44:21 +0200

    added interpolation function for coords

--- client/src/shapes/animation/interpolation/coordinate.ts
+++ client/src/shapes/animation/interpolation/coordinate.ts
@@ -1,8 +1,22 @@
-import type { EasingFunction } from "@utils/animate";
-import type { CoordinateKeyframe, InterpolationFunction } from "./types";
+import { interpolateNumber } from "./number";
+import type { InterpolationFunction, NumericKeyframe } from "./types";
 import type { Coordinate } from "@shape/types/utility";
-
 
 export const interpolateCoordinate: InterpolationFunction<
   Coordinate
-> = (keyframes, easing, fallback) => (progress) => { }
+> = (keyframes, easing, fallback) => (progress) => {
+  const xKeyframes = keyframes.map((kf): NumericKeyframe => ({
+    value: kf.value.x,
+    progress: kf.progress,
+  }))
+
+  const yKeyframes = keyframes.map((kf): NumericKeyframe => ({
+    value: kf.value.y,
+    progress: kf.progress,
+  }))
+
+  return {
+    x: interpolateNumber(xKeyframes, easing, fallback.x)(progress),
+    y: interpolateNumber(yKeyframes, easing, fallback.y)(progress),
+  }
+}
