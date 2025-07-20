b9dd22e9bd061e13ebc5de2c1fad752f55d03c7c
yonava <yonavossandreae@gmail.com>
2025-07-20 14:23:48 -0400

    reduce syntax

--- client/src/products/binary-trees/MainView.vue
+++ client/src/products/binary-trees/MainView.vue
@@ -28,15 +28,8 @@
     graph.focus.reset();
   };
 
-  graph.settings.value.shortcutUndo = () => {
-    if (simRunner.value) return;
-    tree.undo();
-  };
-
-  graph.settings.value.shortcutRedo = () => {
-    if (simRunner.value) return;
-    tree.redo();
-  };
+  graph.settings.value.shortcutUndo = () => !simRunner.value && tree.undo();
+  graph.settings.value.shortcutRedo = () => !simRunner.value && tree.redo();
 
   useEscSimulationShortcut(() => simRunner.value?.stop());
 </script>
