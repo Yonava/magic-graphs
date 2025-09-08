89ab0fa7af8aec31dcdf47764d6ef99351c6440f
teykamp <thomas@eykamp.com>
2025-09-08 10:33:10 -0400

    change color to black

--- packages/products/src/shared/ui/general/dev/BenchmarkingMetrics.vue
+++ packages/products/src/shared/ui/general/dev/BenchmarkingMetrics.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-  import { useFPS } from "@magic/utils/fps";
+import { useFPS } from "@magic/utils/fps";
 
-  const { fps, slowFrameCount, slowFrameRatio } = useFPS();
+const { fps, slowFrameCount, slowFrameRatio } = useFPS();
 </script>
 
 <template>
-  <div class="text-white text-end">
+  <div class="text-black text-end">
     <div>FPS: {{ fps }}</div>
     <div>Slow Frame Count: {{ slowFrameCount }}</div>
     <div>Slow Frame Ratio: {{ slowFrameRatio.toFixed(2) }}</div>
--- packages/products/src/shared/ui/general/dev/GraphAtMousePositionData.vue
+++ packages/products/src/shared/ui/general/dev/GraphAtMousePositionData.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-  import { nonNullGraph as graph } from "@magic/products/shared/globalGraph";
+import { nonNullGraph as graph } from "@magic/products/shared/globalGraph";
 
-  import { computed } from "vue";
+import { computed } from "vue";
 
-  const coords = computed(() => {
-    const {
-      coords: { x, y },
-    } = graph.value.graphAtMousePosition.value;
-    return { x: Math.round(x), y: Math.round(y) };
-  });
+const coords = computed(() => {
+  const {
+    coords: { x, y },
+  } = graph.value.graphAtMousePosition.value;
+  return { x: Math.round(x), y: Math.round(y) };
+});
 
-  const hoveredItems = computed(() => {
-    const { items } = graph.value.graphAtMousePosition.value;
-    return items.map(
-      (item) => `${item.graphType} - ${item.shape.name} (${item.id})`
-    );
-  });
+const hoveredItems = computed(() => {
+  const { items } = graph.value.graphAtMousePosition.value;
+  return items.map(
+    (item) => `${item.graphType} - ${item.shape.name} (${item.id})`
+  );
+});
 </script>
 
 <template>
-  <div class="text-white text-end">
+  <div class="text-black text-end">
     <div>Cursor At: (X = {{ coords.x }}, Y = {{ coords.y }})</div>
     <div>Items Hovered: {{ hoveredItems }}</div>
   </div>
