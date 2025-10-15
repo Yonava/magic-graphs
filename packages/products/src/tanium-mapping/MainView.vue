c191e157666a9467bd1601531b20ab4ac78c780e
yonava <yonavossandreae@gmail.com>
2025-10-15 01:04:03 -0400

    demo

--- packages/products/src/tanium-mapping/MainView.vue
+++ packages/products/src/tanium-mapping/MainView.vue
@@ -23,17 +23,48 @@ const setBigger = (val: boolean) => {
   f();
 }
 
-const blob = computed(() => {
-  return shapes.circle({ id: 't', at: { x: 0, y: 0 }, radius: bigger.value ? 150 : 50, fillColor: bigger.value ? 'blue' : 'red' })
+const blobs = computed(() => {
+  return [[0, 0], [300, 400], [-700, 60], [300, -500], [-500, -450], [-250, 300]].map(([x, y], i) => shapes.circle({
+    id: 'blob' + String(i),
+    at: { x, y },
+    radius: bigger.value ? 75 : 0,
+    fillColor: bigger.value ? colors.RED_600 : 'transparent',
+    // textArea: {
+    //   textBlock: {
+    //     content: 'Large VPC',
+    //     color: bigger.value ? 'white' : 'transparent',
+    //     fontWeight: 'bold',
+    //     fontSize: 20
+    //   },
+    //   color: 'transparent'
+    // }
+  }))
 })
 
-canvas.draw.content.value = (ctx) => [blob.value].forEach((el) => el.draw(ctx))
+const satellites = computed(() => {
+  return [[200, 0], [0, 200], [-200, 0], [0, -200], [400, 400], [600, 300], [100, 700], [900, -400], [-800, -700], [-500, 900], [-1200, 300]].map(([x, y], i) => shapes.circle({
+    id: 'node' + String(i),
+    textArea: {
+      textBlock: {
+        content: 'Node ' + i,
+        color: bigger.value ? 'transparent' : 'white',
+        fontSize: bigger.value ? 0 : 8,
+      },
+      color: 'transparent'
+    },
+    at: bigger.value ? { x: 0, y: 0 } : { x: x / 4, y: y / 4 },
+    radius: bigger.value ? 75 : 20,
+    fillColor: bigger.value ? colors.RED_600 : (y > -500 && x > -500 ? (y > 0 && x > 0 ? colors.RED_800 : colors.ORANGE_700) : colors.PURPLE_700)
+  }))
+})
+
+canvas.draw.content.value = (ctx) => [...blobs.value, ...satellites.value].forEach((el) => el.draw(ctx))
 
 canvas.draw.backgroundPattern.value = (ctx, at, alpha) => cross({
   at,
   size: 12,
   lineWidth: 1,
-  fillColor: colors.GRAY_500 + alpha,
+  fillColor: colors.GRAY_600 + alpha,
 }).draw(ctx);
 
 watch(canvas.camera.state.zoom, (newZoom) => {
