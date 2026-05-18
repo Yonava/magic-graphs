e1299c7c5018997f8685cf306f62e06e4dfb092d
yonava <yonavossandreae@gmail.com>
2026-05-17 21:47:29 -0400

    setup ast product

---
+++ packages/products/src/abstract-syntax-trees/MainView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useGraphWithCanvas } from "../shared/useGraphWithCanvas";
+import GraphProduct from "../shared/ui/general/GraphProduct.vue";
+
+import { AST_GRAPH_SETTINGS } from "./settings";
+
+const graphWithCanvas = useGraphWithCanvas(AST_GRAPH_SETTINGS);
+</script>
+
+<template>
+  <GraphProduct v-bind="graphWithCanvas" />
+</template>
