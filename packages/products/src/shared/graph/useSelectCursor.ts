21d13045ca3df3b2238ce4d428aba50053befe01
yonava <yonavossandreae@gmail.com>
2026-06-11 17:20:29 +0200

    added link to interactivity issue

---
+++ packages/products/src/shared/graph/useSelectCursor.ts
@@ -0,0 +1,26 @@
+import { CanvasElement } from '@magic/graph/plugins/canvas/types';
+import { CURSOR } from '@magic/graph/themes/cursor';
+import { useTheme } from '@magic/graph/themes/useTheme';
+
+import { Graph } from '../useGraphWithCanvas.ts';
+
+export const useSelectCursor = (
+  graph: Graph,
+  predicate: (element: CanvasElement) => boolean,
+) => {
+  const { setTheme, removeAllThemes } = useTheme(graph, 'product/select');
+
+  const activate = () => {
+    setTheme('canvas.cursor', () => {
+      const element = graph.canvas.graphUnderCursor.elements.at(-1);
+      if (!element) return CURSOR.DEFAULT;
+      const isSelectable = predicate(element);
+      return isSelectable ? CURSOR.POINTER : CURSOR.DEFAULT;
+    });
+  };
+
+  return {
+    activate,
+    deactivate: removeAllThemes,
+  };
+};
