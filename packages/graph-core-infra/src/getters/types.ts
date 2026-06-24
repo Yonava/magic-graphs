948fc8361e03e015a00066ac9ba0c8f681732e61
yonava <yonavossandreae@gmail.com>
2026-06-24 14:55:56 -0400

    moved getters out of plugin helpers

---
+++ packages/graph-core-infra/src/getters/types.ts
@@ -0,0 +1,42 @@
+import type { Fraction } from 'mathjs';
+import { UnionToIntersection } from 'ts-essentials';
+
+import { CoreEdge, CoreNode } from '../types.ts';
+
+type BaseGetters = {
+  getNode: {};
+  getEdge: {};
+};
+
+export type CoreGetters = {
+  getNode: CoreNode;
+  getEdge: CoreEdge & { weight: Fraction };
+};
+
+export type ResolveGetters<Getters extends Partial<BaseGetters>> = {
+  getNode: 'getNode' extends keyof Getters
+    ? NonNullable<Getters['getNode']>
+    : {};
+  getEdge: 'getEdge' extends keyof Getters
+    ? NonNullable<Getters['getEdge']>
+    : {};
+};
+
+type DistributeResolveGetters<PartialGetters extends Partial<BaseGetters>> =
+  PartialGetters extends PartialGetters
+    ? ResolveGetters<PartialGetters>
+    : never;
+
+export type MergeGetters<Getters extends Partial<BaseGetters>[]> =
+  Getters extends []
+    ? BaseGetters
+    : {
+        [GettersField in keyof BaseGetters]: UnionToIntersection<
+          DistributeResolveGetters<Getters[number]>[GettersField]
+        >;
+      };
+
+export type GraphGetters<Getters extends BaseGetters> = {
+  getNode: (nodeId: string) => Getters['getNode'];
+  getEdge: (edgeId: string) => Getters['getEdge'];
+};
