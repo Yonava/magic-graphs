7b67e2b9e4fe0815655689276222486c5cf06979
yonava <yonavossandreae@gmail.com>
2026-06-24 14:44:30 -0400

    threw out old preset structure

--- packages/graph-plugins/src/canvas/themes/presets/shared/marquee.ts
+++
@@ -1,5 +0,0 @@
-import { CURSOR } from '../../cursor.ts';
-
-export const marqueeEncapsulatedNodeBoxShared = {
-  cursor: CURSOR.POINTER,
-} as const;
--- packages/graph-plugins/src/canvas/themes/presets/shared/text.ts
+++
@@ -1,6 +0,0 @@
-import { TextStyles } from '../../../themes.ts';
-
-export const textDefaults: Omit<TextStyles, 'text' | 'textColor'> = {
-  textSize: 24,
-  textFontWeight: 'bold',
-};
