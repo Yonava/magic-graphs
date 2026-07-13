54526831b89dc62ea6a6616fa70f078a592ec75b
yonava <yonavossandreae@gmail.com>
2026-07-13 00:21:14 -0700

    readability

--- packages/magic-shared/src/component-slot/types.ts
+++ packages/magic-shared/src/component-slot/types.ts
@@ -15,7 +15,5 @@ export type SlotPosition =
 export type ComponentSlot = {
   id: string;
   position: SlotPosition;
-  // TODO may want to enforce that the component doesn't have any required props
-  // as the renderer doesn't have the ability to provide prop data!
   component: Component;
 };
