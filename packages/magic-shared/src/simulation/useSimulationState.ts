6157b2f314838e78359a5092f8ab4c52e8ee8a93
yonava <yonavossandreae@gmail.com>
2026-07-14 16:32:18 -0700

    polish

--- packages/magic-shared/src/simulation/useSimulationState.ts
+++ packages/magic-shared/src/simulation/useSimulationState.ts
@@ -132,11 +132,6 @@ export const useSimulationState = (
       );
     }
 
-    const slots = componentSlotControls.entries.value.filter((s) =>
-      s.id.startsWith('product/'),
-    );
-    for (const _slot of slots) componentSlotControls.remove(_slot.id);
-
     const { frames, playhead } = computeRun(definition);
 
     const setupContext: SetupContext<Frame> = { getCurrentFrame };
