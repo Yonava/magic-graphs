64b6ff2c20159c8ce95d027cd958502a6dbca82c
yonava <yonavossandreae@gmail.com>
2026-07-16 23:05:57 -0700

    imported avl classes

---
+++ packages/magic-products/src/basic-trees/treeToArray.ts
@@ -0,0 +1,36 @@
+import type { TreeNode } from './avl.ts';
+
+/**
+ * the node at the index of the tree or undefined if the tree index is empty
+ */
+export type TreeArray = (TreeNode | undefined)[];
+
+/**
+ * @returns a tree array where the index of the array corresponds to the tree index
+ */
+export const treeToArray = ({
+  root,
+  treeDepth,
+}: {
+  root: TreeNode | undefined;
+  treeDepth: number;
+}) => {
+  const treeIndexToNodeId: TreeArray = [];
+  if (!root) return treeIndexToNodeId;
+
+  let nodesAtDepth: TreeArray = [root];
+
+  for (let i = 0; i <= treeDepth; i++) {
+    const nodesAtNextDepth: TreeArray = [];
+
+    for (const maybeTreeNode of nodesAtDepth) {
+      treeIndexToNodeId.push(maybeTreeNode);
+      nodesAtNextDepth.push(maybeTreeNode?.left);
+      nodesAtNextDepth.push(maybeTreeNode?.right);
+    }
+
+    nodesAtDepth = [...nodesAtNextDepth];
+  }
+
+  return treeIndexToNodeId;
+};
