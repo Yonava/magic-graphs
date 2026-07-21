7859952a3e7e9d31ba3d270f104c37c64c7eb2ca
yonava <yonavossandreae@gmail.com>
2026-07-21 15:10:38 -0700

    enhanced local storage sync

--- packages/magic-shared/src/product/useLocalStorageGraphSync.ts
+++ packages/magic-shared/src/product/useLocalStorageGraphSync.ts
@@ -1,3 +1,5 @@
+import { debounce } from '@core/utils/debounce';
+
 import { onMounted } from 'vue';
 
 import { Graph } from '../graph/types.ts';
@@ -5,12 +7,13 @@ import { Graph } from '../graph/types.ts';
 const localStorageKey = (id: string) => 'graph-data-' + id;
 
 export const useLocalStorageGraphSync = (graph: Graph, productId: string) => {
-  const save = () => {
+  const save = debounce(() => {
+    console.log('saving');
     window?.localStorage.setItem(
       localStorageKey(productId),
       JSON.stringify(graph.transit.encode()),
     );
-  };
+  }, 500);
 
   const sync = () => {
     const data = window?.localStorage.getItem(localStorageKey(productId));
@@ -20,6 +23,10 @@ export const useLocalStorageGraphSync = (graph: Graph, productId: string) => {
 
   onMounted(sync);
 
+  graph.events._internal.gettersInvalidation.subscribe(
+    'onGettersInvalidated',
+    save,
+  );
   graph.events.subscribe('onStructureChange', save);
   graph.nodeDrag.events.subscribe('onNodeDrop', save);
 };
