e5a37c04e6db86443ce4060c9510bdeba83013c9
yonava <yonavossandreae@gmail.com>
2026-08-07 17:07:36 -0700

    sets

---
+++ packages/magic-products/src/sets/MainView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import { GraphProduct, useGraphProduct } from '@magic/shared/product';
+
+  useGraphProduct({
+    productId: 'sets',
+  });
+</script>
+
+<template>
+  <GraphProduct />
+</template>
